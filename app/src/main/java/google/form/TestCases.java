34562710bcbef5b4bf1aad500fb3e3edd3cceef8
shekar <rajashekar.shekar970@gmail.com>
2024-03-25 20:14:09 +0530

    first commit

---
+++ app/src/main/java/google/form/TestCases.java
@@ -0,0 +1,36 @@
+package google.form;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TestCases {
+    WebDriver driver;
+    //ChromeDriver driver;
+    public TestCases()
+    {
+        System.out.println("Constructor: TestCases");
+        WebDriverManager.chromedriver().timeout(30).setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    public void endTest()
+    {
+        System.out.println("End Test: TestCases");
+        driver.close();
+        driver.quit();
+
+    }
+
+    
+    public  void testCase01(){
+        System.out.println("Start Test case: testCase01");
+        driver.get("https://www.google.com");
+        System.out.println("end Test case: testCase02");
+    }
+
+
+}
